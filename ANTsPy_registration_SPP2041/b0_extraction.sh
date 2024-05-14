ff242c02e8fd2f401507f1e4aaf9e37583f581f3
leahelene <148911458+leahelene@users.noreply.github.com>
2024-05-14 10:26:11 +0200

    Changing input to latest dwi file
    using the latest dwi file 'dwi_den_unr_pre_unbia.mif', exactly the one on which FODs are based

--- ANTsPy_registration_SPP2041/b0_extraction.sh
+++ ANTsPy_registration_SPP2041/b0_extraction.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # extract mean b0 from dMRI
-for_each * : dwiextract IN/5_dwi/dwi_den_unr_pre.mif -bzero - | mrmath 
+for_each * : dwiextract IN/5_dwi/dwi_den_unr_pre_unbia.mif -bzero - | mrmath 
 -axis 3 - mean IN/2_nifti/b0.nii.gz
 
