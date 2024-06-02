e846b5bf22b13fc730d280c5e61f0f07a862cf13
leahelene <148911458+leahelene@users.noreply.github.com>
2024-06-02 15:23:35 +0200

    Updated path to input / output directory
    Updated MPM registration script by specifying path to input and output directory

--- ANTsPy_registration_SPP2041/New_python_scripts/register_MPM_to_dwi.py
+++ ANTsPy_registration_SPP2041/New_python_scripts/register_MPM_to_dwi.py
@@ -8,10 +8,11 @@ for entry in os.listdir(input_dir):
     entry_path = os.path.join(input_dir, entry)
  
     if os.path.isdir(entry_path):
-        r1 = ants.image_read('/6_mpm/results/mpm/R1.nii')
-        r2s = ants.image_read('/6_mpm/results/mpm/R2s.nii')
-        pd = ants.image_read('/6_mpm/results/mpm/PD.nii')
-        mtsat = ants.image_read('/6_mpm/results/mpm/MTsat.nii')
+        r1 = ants.image_read(entry_path + '/6_mpm/results/mpm/R1.nii')
+        r2s = ants.image_read(entry_path + '/6_mpm/results/mpm/R2s.nii')
+        pd = ants.image_read(entry_path + '/6_mpm/results/mpm/PD.nii')
+        mtsat = ants.image_read(entry_path + '/6_mpm/results/mpm/MTsat.nii')
+        b0 = ants.image_read(entry_path + '/2_nifti/b0.nii.gz') 
 
         transform_list = ants.registration(fixed=b0, moving=r1, type_of_transform='Affine')
         transformed_r1 = ants.apply_transforms(fixed=b0, moving=r1, transformlist=transform_list['fwdtransforms'])
@@ -19,7 +20,7 @@ for entry in os.listdir(input_dir):
         transformed_pd = ants.apply_transforms(fixed=b0, moving=pd, transformlist=transform_list['fwdtransforms'])
         transformed_mtsat = ants.apply_transforms(fixed=b0, moving=mtsat, transformlist=transform_list['fwdtransforms'])
 
-        ants.image_write(transformed_r1, os.path.join(output_dir, 'R1_to_dwi.nii.gz'))
-        ants.image_write(transformed_r2s, os.path.join(output_dir,'R2s_to_dwi.nii.gz'))
-        ants.image_write(transformed_pd, os.path.join(output_dir, 'PD_to_dwi.nii.gz'))
-        ants.image_write(transformed_mtsat, os.path.join(output_dir, 'MTsat_to_dwi.nii.gz'))
+        ants.image_write(transformed_r1, output_dir + 'R1_to_dwi.nii.gz')
+        ants.image_write(transformed_r2s, output_dir + 'R2s_to_dwi.nii.gz')
+        ants.image_write(transformed_pd, output_dir + 'PD_to_dwi.nii.gz')
+        ants.image_write(transformed_mtsat, output_dir + 'MTsat_to_dwi.nii.gz')
