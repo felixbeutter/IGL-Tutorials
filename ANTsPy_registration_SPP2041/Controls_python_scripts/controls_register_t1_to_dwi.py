c0c2cfc7e9338bd266b1d2ac0eb40fe5ed102b23
leahelene <148911458+leahelene@users.noreply.github.com>
2024-06-02 21:27:57 +0200

    Updated Control scripts to create new output directory
    Modified Control scripts in order to create a new output directory named antsreg_output to store resulting files

---
+++ ANTsPy_registration_SPP2041/Controls_python_scripts/controls_register_t1_to_dwi.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import ants
+
+input_dir = sys.argv[1]
+
+for entry in os.listdir(input_dir):
+    entry_path = os.path.join(input_dir, entry)
+
+    if os.path.isdir(entry_path):
+        t1 = ants.image_read(os.path.join(entry_path, '2_nifti', 't1.nii.gz'))
+        b0 = ants.image_read(os.path.join(entry_path, '2_nifti', 'b0.nii.gz'))
+
+        mpm_dir = os.path.join(entry_path, '6_mpm', 'results', 'mpm')
+        output_dir = os.path.join(mpm_dir, 'antsreg_output')
+        os.makedirs(output_dir, exist_ok=True)
+
+        transform_list = ants.registration(fixed=b0, moving=t1, type_of_transform='Affine')
+        transformed_t1 = ants.apply_transforms(fixed=b0, moving=t1, transformlist=transform_list['fwdtransforms'])
+        ants.image_write(transformed_t1, os.path.join(output_dir, 't1_to_dwi.nii.gz'))
