33388ff79059fffe9a15d3ef747f258e57971668
leahelene <148911458+leahelene@users.noreply.github.com>
2024-06-05 21:13:26 +0200

    Updated scripts used for AntsReg of Controls
    Replaced t1 variable with mpm

--- ANTsPy_registration_SPP2041/register_MPM_to_dwi.py
+++ ANTsPy_registration_SPP2041/register_MPM_to_dwi.py
@@ -11,10 +11,14 @@ for mod in modalities:
         print(entry_path)
         if os.path.isdir(entry_path) and entry_path.startswith('SPP'):
             if os.path.isfile(entry_path + '/2_nifti/b0.nii.gz') and os.path.isfile(entry_path + '/6_mpm/results/mpm/'+mod+'.nii'):
-                t1 = ants.image_read(entry_path + '/6_mpm/results/mpm/'+mod+'.nii')
+
+                mpm = ants.image_read(entry_path + '/6_mpm/results/mpm/'+mod+'.nii')
                 b0 = ants.image_read(entry_path + '/2_nifti/b0.nii.gz')
+
                 output_dir=entry_path+'/7_cor'
                 os.makedirs(output_dir, exist_ok=True) 
-                transform_list = ants.registration(fixed=b0, moving=t1, type_of_transform='Affine')
-                transformed_t1 = ants.apply_transforms(fixed=b0, moving=t1, transformlist=transform_list['fwdtransforms']) 
-                ants.image_write(transformed_t1, output_dir + '/'+mod+'_to_dwi.nii.gz')
+
+                transform_list = ants.registration(fixed=b0, moving=mpm, type_of_transform='Affine')
+                transformed_mpm = ants.apply_transforms(fixed=b0, moving=mpm, transformlist=transform_list['fwdtransforms']) 
+
+                ants.image_write(transformed_mpm, output_dir + '/'+mod+'_to_dwi.nii.gz')
